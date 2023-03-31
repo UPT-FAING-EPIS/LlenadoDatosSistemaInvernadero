255d2e8e3b5fc6ce924b85775039491108ba6bb3
SebastianCotrina <sebastiancotrina16@gmail.com>
2023-03-31 09:42:48 -0500

    Proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+import PyQt5
+from PyQT5.QtWidgets import QMainWindow
+
+class MainWin(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__()
+
+
+if __name__=='main':
+    print("Iniciando")
