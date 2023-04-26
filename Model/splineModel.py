2036456d4889d0413aab32b75743b65bc6904741
Josue <ja2020067149@virtual.upt.pe>
2023-04-26 11:03:53 -0500

    arreglado

--- Model/splineModel.py
+++ Model/splineModel.py
@@ -3,26 +3,26 @@ from scipy.interpolate import splrep, splev
 from scipy.optimize import bisect
 
 class SplineModel:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.spl = splrep(x, y)
-    
-    def evaluate_spline(self, x_new):
-        return splev(x_new, self.spl)
-    
-    def find_roots(self, y_punto):
-        # Función que devuelve la diferencia entre el valor del spline en un punto x y un valor y_punto dado
-        def f(x, y_punto, spl):
-            return splev(x, spl) - y_punto
+    def __init__(self, time, temperature):
+        self.time = time
+        self.temperature = temperature
+        self.spl = splrep(time, temperature)
 
-        # Buscar los valores de x donde el spline es igual a y_punto
-        raices = []
-        for i in range(len(self.x)-1):
-            a = self.x[i]
-            b = self.x[i+1]
-            if f(a, y_punto, self.spl)*f(b, y_punto, self.spl) < 0:  # Verificar si hay una raíz en el intervalo [a, b]
-                raiz = bisect(f, a, b, args=(y_punto, self.spl))  # Buscar la raíz usando bisect
-                raices.append(raiz)
+    def evaluate_spline(self, time_new):
+        return splev(time_new, self.spl)
 
-        return raices
+    def find_temperature(self, time_point):
+        # Function that returns the difference between the value of the spline at a time point and a given temperature value
+        def f(temperature, time_punto, spl):
+            return splev(time_punto, spl) - temperature
+
+        # Find the temperature at the given time point
+        temperature = None
+        for i in range(len(self.time)-1):
+            a = self.time[i]
+            b = self.time[i+1]
+            if a <= time_point <= b:
+                temperature = bisect(f, self.temperature[i], self.temperature[i+1], args=(time_point, self.spl))
+                break
+
+        return temperature
