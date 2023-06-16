d586ecb35956298525dab36cf8d66ffb485bd142
Sergio Lizárraga <90207380+SergioUPT@users.noreply.github.com>
2023-06-15 19:01:43 -0500

    Update api.py

--- api.py
+++ api.py
@@ -5,71 +5,75 @@ from Controller.mainController import SplineController
 import requests
 from Database.database import Database
 from flask_wtf.csrf import CSRFProtect
-db = Database('localhost', 'root', '', 'invernadero')
 
-app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
-swagger_config = {
-    "headers": [],
-    "specs": [
-        {
-            "endpoint": "apispec_1",
-            "route": "/apispec_1.json",
-            "rule_filter": lambda rule: True,
-            "model_filter": lambda tag: True,
+class TemperatureAPI:
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.csrf = CSRFProtect()
+        self.db = Database('localhost', 'root', '', 'invernadero')
+        self.swagger_config = {
+            "headers": [],
+            "specs": [
+                {
+                    "endpoint": "apispec_1",
+                    "route": "/apispec_1.json",
+                    "rule_filter": lambda rule: True,
+                    "model_filter": lambda tag: True,
+                }
+            ],
+            "static_url_path": "/flasgger_static",
+            "swagger_ui": True,
+            "specs_route": "/apidocs/",
         }
-    ],
-    "static_url_path": "/flasgger_static",
-    "swagger_ui": True,
-    "specs_route": "/apidocs/",
-}
 
-with open("swagger_template.json", "r") as f:
-    template = json.load(f)
+        with open("swagger_template.json", "r") as f:
+            self.template = json.load(f)
 
-swagger = Swagger(app, config=swagger_config, template=template)
+        self.swagger = Swagger(self.app, config=self.swagger_config, template=self.template)
 
-controller = SplineController()
-stored_data = None
+        self.controller = SplineController()
+        self.stored_data = None
 
-@app.route('/api/temperature', methods=['POST'])
-def get_temperature():
-    
-    input_data = request.json
-    time_values = input_data['time_values']
-    temperature_values = input_data['temperature_values']
-    time_points = input_data['time_points']
+        self.csrf.init_app(self.app)
 
-    result = controller.find_temperatures(time_values, temperature_values, time_points)
+        self.app.route('/api/temperature', methods=['POST'])(self.get_temperature)
+        self.app.route('/api/stored_temperature', methods=['GET'])(self.get_stored_temperature)
 
-    
-    stored_data = result.tolist()
-    for i in range(len(time_points)):
-      
-        db.insert_temperature(time_points[i], stored_data[i])
-    return jsonify({"temperature": stored_data})
+    def run(self):
+        self.app.run(debug=True)
+
+    def get_temperature(self):
+        input_data = request.json
+        time_values = input_data['time_values']
+        temperature_values = input_data['temperature_values']
+        time_points = input_data['time_points']
+
+        result = self.controller.find_temperatures(time_values, temperature_values, time_points)
+
+        self.stored_data = result.tolist()
+        for i in range(len(time_points)):
+            self.db.insert_temperature(time_points[i], self.stored_data[i])
+        return jsonify({"temperature": self.stored_data})
+
+    def get_stored_temperature(self):
+        # Make a request to the other API to get the temperature data
+        response = requests.post('http://127.0.0.1:5000/api/temperature', json={
+            'time_values': [1, 2, 3, 4],
+            'temperature_values': [10, 20, 30, 40],
+            'time_points': [1.5, 2.5, 3.5]
+        })
+
+        if response.status_code != 200:
+            return jsonify({"error": "Failed to retrieve temperature data from other API"}), response.status_code
+
+        self.stored_data = response.json()['temperature']
+
+        if self.stored_data is None:
+            return jsonify({"error": "No data available"}), 404
+
+        return jsonify({"temperature": self.stored_data})
 
 
-@app.route('/api/stored_temperature', methods=['GET'])
-def get_stored_temperature():
-    global stored_data
-
-    # Make a request to the other API to get the temperature data
-    response = requests.post('http://127.0.0.1:5000/api/temperature', json={
-        'time_values': [1, 2, 3, 4],
-        'temperature_values': [10, 20, 30, 40],
-        'time_points': [1.5, 2.5, 3.5]
-    })
-
-    if response.status_code != 200:
-        return jsonify({"error": "Failed to retrieve temperature data from other API"}), response.status_code
-
-    stored_data = response.json()['temperature']
-
-    if stored_data is None:
-        return jsonify({"error": "No data available"}), 404
-
-    return jsonify({"temperature": stored_data})
 if __name__ == '__main__':
-    app.run(debug=True)
+    api = TemperatureAPI()
+    api.run()
